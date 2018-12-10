079cba6f5d609fe1f75a3f9539407fe56a35e4f7
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-12-10 17:47:08 -0500

    Mon Dec 10 17:47:08 EST 2018

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/gus-maurizio/structures/circularbuffer"
+	"time"
 )
 
+type measure struct {
+	timestamp	int64
+	message		string
+}
+	
 func main() {  
 	c := circularbuffer.New(5)
 	for i := 1; i <= 21 ; i++ {
-		old := c.Push(i)
+		msr := measure{ timestamp: time.Now().UnixNano(),
+				message:   fmt.Sprintf("this is time %v",time.Now()),
+				}
+		old := c.Push(msr)
 		fmt.Printf("iter %03d %v %+v \n", i, old, c.GetValues())
 	}
-	fmt.Printf("%+v \n", *c)
+	fmt.Printf("%#v \n", c.GetValues()[0:4])
 }
